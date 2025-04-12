8b84ca0d9ab2914a463c431f93a65c14bcc809a0
Hafsa_Rashid <hafsa20cse028@gmail.com>
2025-04-13 01:06:17 +0600

    update

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aq8mwv9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -17,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -25,53 +25,101 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     const userCollection = client.db("tuitionNetworkDB").collection("users");
+    const tutorRequestCollection = client
+      .db("tuitionNetworkDB")
+      .collection("tutorRequests");
 
+    // auth related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      // console.log('user for token', user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
 
+    // Users related API
 
-    app.get('/users', async (req, res) => {
-      
+    const verifyToken = (req, res, next) => {
+      // console.log('inside verifyToken', req.headers.authorization);
+      // next();
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    app.get("/users", async (req, res) => {
       const users = await userCollection.find().toArray();
       res.send(users);
-    
-  });
+    });
 
-
-  app.get('/users/:email', async (req, res) => {
-    try {
+    app.get("/users/:email", async (req, res) => {
+      try {
         const email = req.params.email;
         const user = await userCollection.findOne({ email });
 
         if (user) {
-            res.send({ role: user.role });
+          res.send(user);
         } else {
-            res.status(404).send({ message: "User not found" });
+          res.status(404).send({ message: "User not found" });
         }
-    } catch (error) {
+      } catch (error) {
         console.error(error);
         res.status(500).send({ message: "Internal server error" });
-    }
-});
-
-    app.post('/users', async (req, res) => {
+      }
+    });
+    // Post new user
+    app.post("/users", async (req, res) => {
       const user = req.body;
-      const query = {email: user.email}
+      const query = { email: user.email };
       const existingUser = await userCollection.findOne(query);
-      if(existingUser){
-        return res.send({message: 'user already exists', insertedId: null})
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
       }
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
+
+    app.get("/tutorRequests", async (req, res) => {
+      const result = await tutorRequestCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Post tutor request
+    app.post("/tutorRequests", async (req, res) => {
+      try {
+        const tutorRequest = req.body;
+        const result = await tutorRequestCollection.insertOne(tutorRequest);
+        res.status(201).send({
+          message: "Tutor request submitted successfully",
+          insertedId: result.insertedId,
+        });
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Error submitting tutor request" });
+      }
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 app.get("/", (req, res) => {
   res.send("Welcome to the server");
